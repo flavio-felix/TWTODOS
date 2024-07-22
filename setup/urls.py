86402cdde4ade35420aaabad6d5a3f31aa802a31
flavio-felix <desenhista.felix@yahoo.com.br>
2024-07-22 18:21:29 -0400

    Commit 20240722_2

--- setup/urls.py
+++ setup/urls.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
 from django.urls import path
-from todos.views import todo_list
+
+from todos.views import TodoListView, TodoCreateView
+
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", todo_list),
+    path("", TodoListView.as_view()),
+    path("create", TodoCreateView.as_view())
 ]
